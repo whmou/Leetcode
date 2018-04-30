00bf5c008b48054ba95f3d0f1780beaa8c42188b
wmou <wmou@yahoo-inc.com>
2018-04-30 17:43:00 +0800

    simple str operations

---
+++ str/771_Jewels_and_Stones.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        cnt = 0
+        for s in S:
+            if s in J:
+                cnt += 1
+        return cnt
