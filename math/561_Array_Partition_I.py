1ce23d0fc2c6c76060e8e1194c2551b0c1f55c09
whmou <wmou@yahoo-inc.com>
2018-05-05 00:08:36 +0800

    arr partition

---
+++ math/561_Array_Partition_I.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return sum([val for i, val in enumerate(nums) if i % 2 == 0])
