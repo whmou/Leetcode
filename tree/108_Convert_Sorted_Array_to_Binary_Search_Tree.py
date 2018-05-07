b8d01e8cc37cc04a78ab2c3c46c33059b55b5c6f
whmou <whmou0@gmail.com>
2018-05-07 23:51:57 +0800

    hieght balanced sorted binary tree

---
+++ tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if nums is None or len(nums)==0:
+            return None
+        mid = len(nums)/2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
